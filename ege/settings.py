1a8e6f9ddef991fc2fab8b79efc2d8d51084123a
Mikhail Teyblyum <misha@tejblum.pp.ru>
2022-11-18 15:52:51 +0300

    settings 2

--- ege/settings.py
+++ ege/settings.py
@@ -124,8 +124,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = []
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/static/'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
