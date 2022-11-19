18d671cbe8d260a3fa492eeeedda87c4cc940679
Mikhail Teyblyum <misha@tejblum.pp.ru>
2022-11-19 12:55:13 +0300

    static

--- ege/settings.py
+++ ege/settings.py
@@ -118,16 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main/static/'
 
 # mmt
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, '/main/static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
