96431caf3f8b71bed786b0f61bb6a3b738b7b2b6
Mikhail Teyblyum <misha@tejblum.pp.ru>
2024-02-10 22:56:24 +0300

    tkinter 3

--- main/views.py
+++ main/views.py
@@ -5,8 +5,8 @@ from .models import Bimg
 from .models import Themes
 from .models import Smailic
 from .models import Bpdf
-from ctypes import *
-import sys
+from tkinter import *
+
 import socket
 #from screeninfo import get_monitors
 
@@ -71,8 +71,7 @@ def variant(request, pkk, e_tt):
 
 def index(request):
 
-    print(windll.user32.GetSystemMetrics(0))
-    print(windll.user32.GetSystemMetrics(1))
+    root = Tk()
 
 #    monitor = get_monitors()[0]
 #    print('Width: %d, Height: %d' % (monitor.width, monitor.height))
